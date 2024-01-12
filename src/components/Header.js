001492dc613ada412e4c7d8f406c8e951ba373e3
Ashutosh Tripathi <ramtripathi246@gmail.com>
2024-01-13 03:13:16 +0530

    Header Component created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,27 @@
-W3
+const Title = () => (
+  <a href="/">
+    <img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw7klc_4cnckngeiQbTRTX-9u87a-8sU5OLQ&usqp=CAU"
+      alt="logo"
+      className="logo"
+    />
+  </a>
+);
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <nav className="navbar-item">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
